5b28b6971457afb017a4fa08fb8a412b44cddf3b
BilledTrain380 <nicolas.maerchy@me.com>
2017-02-15 16:14:58 +0100

    Implement unit test IOMascotProvider::getAvailableMascots.

--- src/main/java/com/group_finity/mascot/dao/IOMascotProvider.java
+++ src/main/java/com/group_finity/mascot/dao/IOMascotProvider.java
@@ -1,10 +1,16 @@
 package com.group_finity.mascot.dao;
 
 import com.group_finity.mascot.dto.BasicMascot;
+import com.group_finity.mascot.dto.MascotConfiguration;
 import com.group_finity.mascot.dto.PreviewMascot;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * A mascot provider for the file system.
@@ -22,6 +28,41 @@ public class IOMascotProvider implements MascotProvider {
      */
     @Override
     public Set<PreviewMascot> getAvailableMascots(Path directory) {
+        
+        try(Stream<Path> pathStream = Files.list(directory)) {
+            
+            Set<PreviewMascot> mascots = new HashSet<>();
+            
+            pathStream.forEach((mascotDir) -> {
+
+                String actionFilePath = getClass().getClassLoader().getResource("conf/actions.xml").getFile();
+                File actionFile = new File(actionFilePath);
+                
+                String behaviorFilePath = getClass().getClassLoader().getResource("conf/behaviors.xml").getFile();
+                File behaviorFile = new File(behaviorFilePath);
+                
+                Path imgPath = mascotDir.resolve("shime1.png");
+
+                MascotConfiguration config = new MascotConfiguration();
+                config.setActionFile(actionFile);
+                config.setBahaviorFile(behaviorFile);
+
+                PreviewMascot mascot = new PreviewMascot();
+                mascot.setName(mascotDir.getFileName().toString());
+                mascot.setConfig(config);
+                mascot.setImage(imgPath.toFile());
+                
+                mascots.add(mascot);
+                
+            });
+            
+            return mascots;
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         throw new UnsupportedOperationException("This method is not implemented yet.");
     }
 
